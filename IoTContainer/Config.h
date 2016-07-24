e2c328ece206afa3f136968a4dec59e8011aeeec
tinkermax <malcolm@yeoman.com.au>
2016-07-24 21:13:03 +1000

    Create Config.h

---
+++ IoTContainer/Config.h
@@ -0,0 +1,18 @@
+//Wifi station name:
+const char* ssid = "xxx";
+
+//Wifi Password:
+const char* password = "xxx";
+
+//MQTT broker: leave as-is for Adafruit IO
+const char* mqtt_server = "io.adafruit.com";
+
+//Your Adafruit Username:
+const char* mqttuser = "xxx";
+
+//Your secret AIO key:
+const char* mqttpwd = "xxx";
+
+//URL containing <Name of your Maker Event> and <Your maker channel key>
+//Note don't include the < and >
+String iftttMakerUrl = "https://maker.ifttt.com/trigger/<Name of your Maker Event>/with/key/<Your Maker Channel Key>";
